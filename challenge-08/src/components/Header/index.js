882dc2a73f28f28c6392fba0dd02fac04320d78c
Bruno Martins Crocomo <bruno.crocomo@gmail.com>
2019-09-09 12:12:32 -0300

    Challenge 08 resolution

---
+++ challenge-08/src/components/Header/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import {
+    Wrapper,
+    Container,
+    LogoContainer,
+    Logo,
+    BasketContainer,
+    ItemCount,
+} from './styles';
+
+export default function Header({ navigation }) {
+    const cartSize = useSelector(state => state.cart.length);
+    return (
+        <Wrapper>
+            <Container>
+                <LogoContainer onPress={() => navigation.navigate('Main')}>
+                    <Logo />
+                </LogoContainer>
+                <BasketContainer onPress={() => navigation.navigate('Cart')}>
+                    <Icon name="shopping-basket" color="#FFF" size={24} />
+                    <ItemCount highlight={cartSize > 0}>
+                        {cartSize || 0}
+                    </ItemCount>
+                </BasketContainer>
+            </Container>
+        </Wrapper>
+    );
+}
